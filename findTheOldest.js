943941dd7ebf521497c01a9fd5c0c2e87cc425b1
nexus0208 <christiandelacruz0208@gmail.com>
2020-08-19 14:54:43 +0800

    .

--- findTheOldest.js
+++ findTheOldest.js
@@ -1,23 +1,3 @@
-const people = [
-  {
-    name: 'Carly',
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
-  },
-  {
-    name: 'Ray',
-    yearOfBirth: 1962,
-    yearOfDeath: 2011
-  },
-  {
-    name: 'Jane',
-    yearOfBirth: 1912,
-    yearOfDeath: 1941
-  },
-];
-
-
-
 let findTheOldest = function (people) {
   const age = (x) => (x.yearOfDeath || new Date().getFullYear()) - x.yearOfBirth;
   const oldest = people.sort((a, b) =>
@@ -26,4 +6,5 @@ let findTheOldest = function (people) {
   return oldest[0];
 };
 
-console.log(findTheOldest(people));
+
+module.exports = findTheOldest
